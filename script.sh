5a8d592bfd1b1356adbe3e33d71c853386732d5d
requeiem <72945139+requeiem@users.noreply.github.com>
2024-05-11 15:41:24 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,16 @@ echo 'Запускаю скрипт'
 echo 'Изменяю директорию на /opt'
 cd /opt || exit 1
 
+echo 'Установка Docker'
+sudo apt-get update || exit 1
+sudo apt-get install ca-certificates curl -y || exit 1
+sudo install -m 0755 -d /etc/apt/keyrings || exit 1
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc || exit 1
+sudo chmod a+r /etc/apt/keyrings/docker.asc || exit 1
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || exit 1
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y || exit 1
+sudo docker run hello-world || exit 1
+
 echo 'Копирую репозиторий со сборкой'
 sudo git clone https://github.com/requeiem/shvirtd-example-python.git || exit 1
 
@@ -14,11 +24,7 @@ sudo apt install python3.10-venv -y || exit 1
 sudo apt install python3-pip -y || exit 1
 sudo python3 -m venv venv || exit 1
 source venv/bin/activate || exit 1
-
-echo 'Устанавливаю зависимости'
-sudo pip install -r requirements.txt || exit 1
-
-echo 'Запускаю приложение'
-nohup python main.py > /dev/null 2>&1 &
-
-echo 'Процесс запущен в фоновом режиме'
+sudo docker compose build || exit 1
+sudo docker compose up -d || exit 1
+echo docker ps -a || exit 1
+echo "Готово!"
